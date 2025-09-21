c0c3ba7ef385c45cec3f50b6fc70088ef4164072
roman <galas@incust.com>
2025-09-21 18:50:31 +0300

    Task 2 improved

--- tasks/task_02.py
+++ tasks/task_02.py
@@ -91,8 +91,21 @@ def test_integrate() -> None:
     a = 0.0
     b = 2.0
 
+    plot_integrate_function(f, a, b)
+
+    # Integration using scipy.integrate.quad
+    print("#", "=" * 100, "#")
+    print("Etalon integral value calculated using scipy.integrate.quad")
+    etalon_integral_value, etalon_se_value = spi.quad(f, a, b)
+    print(
+        f"Result of integration using scipy.integrate.quad: {etalon_integral_value} "
+        f"(estimate of the absolute error: {etalon_se_value:.15f})"
+    )
+    print("#", "=" * 100, "#")
+
     # Integration using Monte Carlo method
-    for points_number in {1_000, 10_000, 100_000}:
+    experiments_number = 100
+    for points_number in [10, 100, 1_000, 10_000, 100_000]:
         integral_value, se_value = integrate_by_monte_carlo(f, a, b, points_number=points_number)
         print(
             f"Result of integration using Monte Carlo method: {integral_value} "
@@ -100,29 +113,38 @@ def test_integrate() -> None:
             f" - number of random points {points_number}, number of experiments 1"
         )
 
-    experiments_number = 100
-    points_number = 10_000
-    integral_value = None
-    with ProcessPoolExecutor() as executor:
-        futures = [
-            executor.submit(integrate_by_monte_carlo, f, a, b, points_number=points_number)
-            for _ in range(experiments_number)
-        ]
-        integral_values: list[float] = [future.result()[0] for future in futures]
-        se_values: list[float] = [future.result()[1] for future in futures]
-        integral_value = np.mean(integral_values)
-        se_value = np.mean(se_values)
-    print(
-        f"Result of integration using Monte Carlo method: {integral_value} "
-        f"(estimate of the absolute error: {se_value:.15f})"
-        f"(number of random points {points_number}, number of experiments {experiments_number})"
-    )
+        with ProcessPoolExecutor() as executor:
+            futures = [
+                executor.submit(integrate_by_monte_carlo, f, a, b, points_number=points_number)
+                for _ in range(experiments_number)
+            ]
+            integral_values: list[float] = [future.result()[0] for future in futures]
+            se_values: list[float] = [future.result()[1] for future in futures]
+            integral_value = np.mean(integral_values)
+            se_value = np.mean(se_values)/np.sqrt(experiments_number)
 
-    # Integration using scipy.integrate.quad
-    print(
-        "Result of integration using scipy.integrate.quad: {0} (estimate of the absolute error: {1:.15f})".format(
-            *spi.quad(f, a, b)
-        )
-    )
+            print(
+                f"Result of integration using Monte-Carlo method: {integral_value} "
+                f"(estimate of the absolute error: {se_value:.15f})"
+                f"(number of random points {points_number}, number of experiments {experiments_number})"
+            )
 
-    plot_integrate_function(f, a, b)
+    # Visualization of the dependence of the integral value calculation by the Monte Carlo method on the number of points
+    integral_values: list[float] = []
+    se_values: list[float] = []
+    points_numbers = np.unique(np.logspace(np.log10(10), np.log10(1_000_000), num=1_000).astype(int))
+    for points_number in points_numbers:
+        integral_value, se_value = integrate_by_monte_carlo(f, a, b, points_number=points_number)
+        integral_values.append(integral_value)
+        se_values.append(se_value)
+
+    plt.figure(figsize=(16, 6))
+    plt.errorbar(points_numbers, integral_values, yerr=se_values, fmt="o-", capsize=5, label="MC integral value ± SE")
+    plt.axhline(etalon_integral_value, color="red", linestyle="--", label="scipy.integrate.quad integral value")
+    plt.xscale("log")
+    plt.xlabel("N (Points Number)")
+    plt.ylabel("Integral Value ± SE")
+    plt.title("Integration using Monte-Carlo (MC) method by N")
+    plt.legend()
+    plt.grid(True, which="both", linestyle="--", alpha=0.6)
+    plt.show()
